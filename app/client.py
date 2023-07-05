48ee55d866980f5678ed46a94efa5088d4a340ae
Sergey Mytsykov <smytsykov@gmail.com>
2023-07-05 14:30:57 +0300

    задание 10

--- app/client.py
+++ app/client.py
@@ -3,103 +3,101 @@ import threading
 import time
 import sys
 from common import vars
-from common.utils import get_message, send_message, get_config_data
+from common.abstract_messenger import AbstractMessenger
 import logging
 import loggers.client_logs
 from deorators.call_logger import CallLogger
 
 
-logger = logging.getLogger('app.client')
+class Client(AbstractMessenger):
+    account_name: str
+    sock: socket
+    logger: logging.Logger
 
+    def __init__(self):
+        self.logger = logging.getLogger('app.client')
+        self.get_config_data()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.listen_host, self.listen_port))
+        self.logger.info('connected to server')
 
-@CallLogger()
-def create_presence(sock: socket, account_name='Guest'):
-    logger.debug(f'Create presence message account_name: {account_name}')
-    send_message(sock, {
-        vars.ACTION: vars.PRESENCE,
-        vars.TIME: time.time(),
-        vars.USER: {
-            vars.ACCOUNT_NAME: account_name
-        }
-    })
-
-
-@CallLogger()
-def create_meesage(sock, account_name='Guest'):
-    while True:
-        destination = input('Введите пользователя или all для отправки или \'@q\' для завершения работы: ')
-        message_text = input('Введите сообщение для отправки или \'@q\' для завершения работы: ')
-        if message_text == '@q':
-            sock.close()
-            logger.info('Завершение работы по команде пользователя.')
-            print('Спасибо за использование нашего сервиса!')
-            sys.exit(0)
-        logger.debug(f'Create text message account_name: {account_name}')
-        send_message(sock, {
-            vars.ACTION: vars.MESSAGE,
+    @CallLogger()
+    def create_presence(self):
+        self.logger.debug(f'Create presence message account_name: {self.account_name}')
+        self.send_message(self.sock, {
+            vars.ACTION: vars.PRESENCE,
             vars.TIME: time.time(),
             vars.USER: {
-                vars.ACCOUNT_NAME: account_name
-            },
-            vars.MESSAGE_TEXT: message_text,
-            vars.DESTINATION: destination,
+                vars.ACCOUNT_NAME: self.account_name
+            }
         })
 
+    @CallLogger()
+    def create_meesage(self):
+        while True:
+            destination = input('Введите пользователя или all для отправки или \'@q\' для завершения работы: ')
+            message_text = input('Введите сообщение для отправки или \'@q\' для завершения работы: ')
+            if message_text == '@q':
+                self.sock.close()
+                self.logger.info('Завершение работы по команде пользователя.')
+                print('Спасибо за использование нашего сервиса!')
+                sys.exit(0)
+            self.logger.debug(f'Create text message account_name: {self.account_name}')
+            self.send_message(self.sock, {
+                vars.ACTION: vars.MESSAGE,
+                vars.TIME: time.time(),
+                vars.USER: {
+                    vars.ACCOUNT_NAME: self.account_name
+                },
+                vars.MESSAGE_TEXT: message_text,
+                vars.DESTINATION: destination,
+            })
 
-@CallLogger()
-def receive_message(sock: socket):
-    while True:
-        message = get_message(sock)
-        if vars.RESPONSE in message and message[vars.RESPONSE] == 200:
-            logger.info(f'Получено сообщение 200')
-        elif vars.ACTION in message and message[vars.ACTION] == vars.MESSAGE and \
-                vars.USER in message and vars.MESSAGE_TEXT in message:
-            logger.info(f'Получено сообщение от пользователя {message[vars.USER][vars.ACCOUNT_NAME]}: {message[vars.MESSAGE_TEXT]}')
-        else:
-            logger.error(f'Получено некорректное сообщение с сервера: {message}')
+    @CallLogger()
+    def receive_message(self):
+        while True:
+            message = self.get_message(self.sock)
+            if vars.RESPONSE in message and message[vars.RESPONSE] == 200:
+                self.logger.info(f'Получено сообщение 200')
+            elif vars.ACTION in message and message[vars.ACTION] == vars.MESSAGE and \
+                    vars.USER in message and vars.MESSAGE_TEXT in message:
+                self.logger.info(
+                    f'Получено сообщение от пользователя {message[vars.USER][vars.ACCOUNT_NAME]}: {message[vars.MESSAGE_TEXT]}')
+            else:
+                self.logger.error(f'Получено некорректное сообщение с сервера: {message}')
 
+    @CallLogger()
+    def process_answer(self, message: dict):
+        self.logger.debug(f'receive new message {message}')
+        if vars.RESPONSE in message:
+            if message[vars.RESPONSE] == 200:
+                self.logger.debug('message ok')
+                return '200: OK'
+            self.logger.error('message fail')
+            return f'400: {message[vars.ERROR]}'
+        raise ValueError
 
-@CallLogger()
-def process_answer(message: dict):
-    logger.debug(f'receive new message {message}')
-    if vars.RESPONSE in message:
-        if message[vars.RESPONSE] == 200:
-            logger.debug('message ok')
-            return '200: OK'
-        logger.error('message fail')
-        return f'400: {message[vars.ERROR]}'
-    raise ValueError
+    def runner(self):
+        receiver = threading.Thread(target=self.receive_message)
+        receiver.daemon = True
+        receiver.start()
 
+        sender = threading.Thread(target=self.create_meesage)
+        sender.daemon = True
+        sender.start()
 
-def main():
-    listen_ip, listen_port, client_name = get_config_data()
-    logger.info(f'connecting to server, server ip {listen_ip}, server port {listen_port}')
+        while True:
+            try:
+                time.sleep(1)
+                if receiver.is_alive() and sender.is_alive():
+                    continue
+                break
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((listen_ip, listen_port))
-    logger.info('connected to server')
-    create_presence(client_socket, client_name)
-    logger.debug(f'answer from server: {process_answer(get_message(client_socket))}')
-
-    receiver = threading.Thread(target=receive_message, args=[client_socket])
-    receiver.daemon = True
-    receiver.start()
-
-    sender = threading.Thread(target=create_meesage, args=[client_socket, client_name])
-    sender.daemon = True
-    sender.start()
-
-    while True:
-        try:
-            time.sleep(1)
-            if receiver.is_alive() and sender.is_alive():
-                continue
-            break
-
-        except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
-            logger.error(f'Соединение с сервером {listen_ip} было потеряно.')
-            sys.exit(1)
+            except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                self.logger.error(f'Соединение с сервером {self.listen_host} было потеряно.')
+                sys.exit(1)
 
 
 if __name__ == '__main__':
-    main()
+    client = Client()
+    client.runner()
--- app/server.py
+++ app/server.py
@@ -1,128 +1,136 @@
-import socket
-import time
-from common import vars
-from common.utils import get_message, send_message, get_config_data, create_response
 import logging
-import select
-import loggers.server_logs
+import time
+from select import select
+import socket
+from common import vars
+from common.abstract_messenger import AbstractMessenger
 from deorators.call_logger import CallLogger
+from common.meta_classes import ServerMaker
 
-logger = logging.getLogger('app.server')
-
-
-@CallLogger()
-def process_client_message(client_sock: socket, wait_messages: list, users: dict):
-    """
-    Получение сообщения от клиента и постановка его в очередь сообщений
-
-    :param users:
-    :param client_sock:
-    :param wait_messages:
-    :return:
-    """
-    message = get_message(client_sock)
-
-    logger.debug(f'receive new message {message}')
-    if vars.ACTION in message and vars.TIME in message and vars.USER in message:
-        # Страшно переходить на версию 3.10 но очень хочется матч кейс
-        if message[vars.ACTION] == vars.PRESENCE:
-            if message[vars.USER][vars.ACCOUNT_NAME] not in users.keys():
-                users[message[vars.USER][vars.ACCOUNT_NAME]] = client_sock
-                send_message(client_sock, create_response())
-            else:
-                send_message(client_sock, create_response('Имя пользователя уже занято.'))
-                client_sock.close()
-
-        elif message[vars.ACTION] == vars.MESSAGE and vars.DESTINATION in message:
-            logger.debug('message ok')
-            wait_messages.append((message[vars.USER], message[vars.MESSAGE_TEXT], message[vars.DESTINATION]))
-            send_message(client_sock, create_response())
-
-        elif message[vars.ACTION] == vars.EXIT:
-            client_sock.close()
-            del users[message[vars.USER][vars.ACCOUNT_NAME]]
-
-        return
-
-    logger.error('message fail')
-    return create_response('Bad request')
+import loggers.server_logs
 
 
-@CallLogger()
-def message_sender(user_name: str, message: dict, users_list: dict, connected_clients: list):
-    try:
-        send_message(users_list[user_name], message)
-    except Exception as e:
-        logger.info(f'Client {users_list[user_name].getpeername()} disconnected. 2 {e}')
-        connected_clients.remove(users_list[user_name])
-        del users_list[user_name]
-
-
-@CallLogger()
-def message_sender_broadcast(send_queue: list, message: dict, connected_clients: list):
-    for waiting_client in send_queue:
-        try:
-            send_message(waiting_client, message)
-        except Exception as e:
-            logger.info(f'Client {waiting_client.getpeername()} disconnected. 1 {e}')
-            connected_clients.remove(waiting_client)
-
-
-def main():
-    listen_ip, listen_port, _ = get_config_data()
-    logger.info(f'server starting, listen ip {listen_ip}, server port {listen_port}')
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.bind((listen_ip, listen_port))
-    transport.listen(vars.MAX_CONNECTIONS)
-    transport.settimeout(0.5)
-
+class Server(AbstractMessenger, metaclass=ServerMaker):
+    transport: socket
     connected_clients = []
     waiting_messages = []
     users_list = dict()
 
-    while True:
-        try:
-            client, client_address = transport.accept()
-        except OSError:
-            pass
-        else:
-            connected_clients.append(client)
-            logger.info(f'accept connection from {client_address}')
+    def __init__(self):
+        self.logger = logging.getLogger('app.server')
+        self.get_config_data()
+        self.logger.info(f'server starting, listen ip {self.listen_host}, server port {self.listen_host}')
 
-        recv_queue = []
-        send_queue = []
-        err_lst = []
-        try:
-            if connected_clients:
-                recv_queue, send_queue, err_lst = select.select(connected_clients, connected_clients, [], 0)
-        except OSError:
-            pass
+    @CallLogger()
+    def init_socket(self):
+        self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.transport.bind((self.listen_host, self.listen_port))
+        self.transport.listen(vars.MAX_CONNECTIONS)
+        self.transport.settimeout(0.5)
 
-        if recv_queue:
-            for waiting_messanger in recv_queue:
-                try:
-                    process_client_message(waiting_messanger, waiting_messages, users_list)
-                except Exception as e:
-                    logger.info(f'Client {waiting_messanger.getpeername()} disconnected. {e}')
-                    connected_clients.remove(waiting_messanger)
+    @CallLogger()
+    def process_client_message(self, client_sock: socket):
+        """
+        Получение сообщения от клиента и постановка его в очередь сообщений
 
-        if waiting_messages and send_queue:
-            for wait_message in waiting_messages:
-                user_name = wait_message[2]
-                message = {
-                    vars.ACTION: vars.MESSAGE,
-                    vars.USER: wait_message[0],
-                    vars.TIME: time.time(),
-                    vars.MESSAGE_TEXT: wait_message[1]
-                }
-                waiting_messages.remove(wait_message)
+        :param users:
+        :param client_sock:
+        :param wait_messages:
+        :return:
+        """
+        message = self.get_message(client_sock)
 
-                if wait_message[2] == vars.DESTINATION_ALL:
-                    message_sender_broadcast(send_queue, message, connected_clients)
+        self.logger.debug(f'receive new message {message}')
+        if vars.ACTION in message and vars.TIME in message and vars.USER in message:
+            # Страшно переходить на версию 3.10 но очень хочется матч кейс
+            if message[vars.ACTION] == vars.PRESENCE:
+                if message[vars.USER][vars.ACCOUNT_NAME] not in self.users_list.keys():
+                    self.users_list[message[vars.USER][vars.ACCOUNT_NAME]] = client_sock
+                    self.send_message(self.create_response())
                 else:
-                    message_sender(user_name, message, users_list, connected_clients)
+                    self.send_message(self.create_response('Имя пользователя уже занято.'))
+                    client_sock.close()
+
+            elif message[vars.ACTION] == vars.MESSAGE and vars.DESTINATION in message:
+                self.logger.debug('message ok')
+                self.waiting_messages.append(
+                    (message[vars.USER], message[vars.MESSAGE_TEXT], message[vars.DESTINATION]))
+                self.send_message(self.create_response())
+
+            elif message[vars.ACTION] == vars.EXIT:
+                client_sock.close()
+                del self.users_list[message[vars.USER][vars.ACCOUNT_NAME]]
+
+            return
+
+        self.logger.error('message fail')
+        return self.create_response('Bad request')
+
+    @CallLogger()
+    def message_sender(self, user_name: str, message: dict):
+        try:
+            self.send_message(self.users_list[user_name], message)
+        except Exception as e:
+            self.logger.info(f'Client {self.users_list[user_name].getpeername()} disconnected. 2 {e}')
+            self.connected_clients.remove(self.users_list[user_name])
+            del self.users_list[user_name]
+
+    @CallLogger()
+    def message_sender_broadcast(self, send_queue: list, message: dict):
+        for waiting_client in send_queue:
+            try:
+                self.send_message(waiting_client, message)
+            except Exception as e:
+                self.logger.info(f'Client {waiting_client.getpeername()} disconnected. 1 {e}')
+                self.connected_clients.remove(waiting_client)
+
+    @CallLogger()
+    def runner(self):
+        self.init_socket()
+
+        while True:
+            try:
+                client, client_address = self.transport.accept()
+            except OSError:
+                pass
+            else:
+                self.connected_clients.append(client)
+                self.logger.info(f'accept connection from {client_address}')
+
+            recv_queue = []
+            send_queue = []
+            err_lst = []
+            try:
+                if self.connected_clients:
+                    recv_queue, send_queue, err_lst = select(self.connected_clients, self.connected_clients, [], 0)
+            except OSError:
+                pass
+
+            if recv_queue:
+                for waiting_messanger in recv_queue:
+                    try:
+                        self.process_client_message(waiting_messanger)
+                    except Exception as e:
+                        self.logger.info(f'Client {waiting_messanger.getpeername()} disconnected. {e}')
+                        self.connected_clients.remove(waiting_messanger)
+
+            if self.waiting_messages and send_queue:
+                for wait_message in self.waiting_messages:
+                    user_name = wait_message[2]
+                    message = {
+                        vars.ACTION: vars.MESSAGE,
+                        vars.USER: wait_message[0],
+                        vars.TIME: time.time(),
+                        vars.MESSAGE_TEXT: wait_message[1]
+                    }
+                    self.waiting_messages.remove(wait_message)
+
+                    if wait_message[2] == vars.DESTINATION_ALL:
+                        self.message_sender_broadcast(send_queue, message)
+                    else:
+                        self.message_sender(user_name, message)
 
 
 if __name__ == '__main__':
-    main()
+    server = Server()
+    server.runner()
